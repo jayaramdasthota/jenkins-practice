67cdcfa9e529712e28bd51f19a7a79227314d643
jayaramdasthota <jayaram114433@gmail.com>
2020-06-08 12:10:32 +0530

    commiting new files to repo..............

---
+++ print.java
@@ -0,0 +1,8 @@
+public class print
+ {
+   public static void main(String[] args)
+	{
+	 System.out.println("Successfully Printing...........");
+	 System.out.println("Ready to Deploy.................");
+	}
+ }
